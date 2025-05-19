13959baef9d112cc58105a19e8d5f295bc6e43cb
Rakshit Katiyar <116247606+Rakshit12902@users.noreply.github.com>
2025-05-19 21:36:13 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,37 @@
 import axios from 'axios';
 
+<<<<<<< HEAD
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Request interceptor
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+// Response interceptor
+api.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response?.status === 401) {
+    localStorage.removeItem('token');
+    window.location.href = '/login';
+  }
+  return Promise.reject(error);
+});
+
+export default api;
+=======
 const api = axios.create({
   baseURL: 'http://localhost:5000/api',
   headers: {
@@ -17,3 +49,4 @@ api.interceptors.request.use(config => {
 });
 
 export default api;
+>>>>>>> a87378f47ac3ca52f51d4a2c04239fe0ccdf12ca
